b335a6042f04e4540a9938cdb9a1e8f809de946a
s4jj4d021 <TgMemberPlus@gmail.com>
2017-03-27 17:17:03 +0430

    Update cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 while true ; do
-  for entr in sinchi-*.sh ; do
+  for entr in tabchi-*.sh ; do
     entry="${entr/.sh/}"
+    tmux kill-session -t $entry
     ./off.sh
     rm -rf ~/.telegram-cli/$entry/data/animation/*
     rm -rf ~/.telegram-cli/$entry/data/audio/*
@@ -12,8 +13,28 @@ while true ; do
     rm -rf ~/.telegram-cli/$entry/data/video/*
     rm -rf ~/.telegram-cli/$entry/data/voice/*
     rm -rf ~/.telegram-cli/$entry/data/profile_photo/*
+    rm -rf ~/.telegram-cli/$entry/data/thumb/*
+    rm -rf ~/.telegram-cli/tabchi-*/data/photo
+    rm -rf ~/.telegram-cli/tabchi-*/data/animation
+    rm -rf ~/.telegram-cli/tabchi-*/data/audio
+    rm -rf ~/.telegram-cli/tabchi-*/data/document
+    rm -rf ~/.telegram-cli/tabchi-*/data/sticker
+    rm -rf ~/.telegram-cli/tabchi-*/data/video
+    rm -rf ~/.telegram-cli/tabchi-*/data/voice
+    rm -rf ~/.telegram-cli/tabchi-*/data/profile_photo
+    rm -rf ~/.telegram-cli/tabchi-*/data/thumb
+    tmux new-session -d -s $entry "./$entr"
+    tmux detach -s $entry
     ./on.sh
   done
-  echo All SinChi is Running!
+   echo -e "\033[38;5;600m"
+   echo -e " :::::::::::  ::::::::     ::::    ::::  :::::::::: ::::    ::::  :::::::::  :::::::::: ::::::::: "
+   echo -e "     :+:     :+:    :+:    +:+:+: :+:+:+ :+:        +:+:+: :+:+:+ :+:    :+: :+:        :+:    :+:"
+   echo -e "     +:+     +:+           +:+ +:+:+ +:+ +:+        +:+ +:+:+ +:+ +:+    +:+ +:+        +:+    +:+"
+   echo -e "     +#+     :#:           +#+  +:+  +#+ +#++:++#   +#+  +:+  +#+ +#++:++#+  +#++:++#   +#++:++#: "
+   echo -e "     +#+     +#+   +#+#    +#+       +#+ +#+        +#+       +#+ +#+    +#+ +#+        +#+    +#+"
+   echo -e "     #+#     #+#    #+#    #+#       #+# #+#        #+#       #+# #+#    #+# #+#        #+#    #+#"
+   echo -e "     ###      ########     ###       ### ########## ###       ### #########  ########## ###    ###"  
+   echo Bots Running!
   sleep 1800
 done
