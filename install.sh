ab67d526cdf2f33160161edf63e36ce6b507931a
s4jj4d021 <TgMemberPlus@gmail.com>
2017-03-27 17:06:14 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,44 @@
-
+sudo apt-get autoclean
+sudo apt-get autoremove
 sudo apt-get update
-
 sudo apt-get upgrade
-
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-
-sudo apt-get install lua-lgi
-
-sudo apt-get install software-properties-common
-
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
-
-sudo apt-get install libstdc++6
-
-sudo apt-get update
-
-sudo apt-get upgrade
-
+sudo apt-get install -f
+sudo dpkg -a --configure
 sudo apt-get dist-upgrade
-
+sudo dpkg --configure -a
+sudo sudo apt-get dist-upgrade
+sudo apt-get install -y tor
+sudo apt-get install -y openvpn
+sudo apt-get install network-manager-openvpn
+sudo apt-get install lua50
+sudo apt-get install lua5.1
+sudo apt-get install lua5.2
+sudo apt-get install lua5.3
+wget "https://valtman.name/files/telegram-cli-1222"
+sudo apt-get install libreadline6 libreadline-dev libreadline-dev libreadline6-dev libconfig-dev libssl-dev tmux lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip redis-server autoconf git g++ libjansson-dev libpython-dev expat libexpat1-dev ppa-purge python3-pip python3-dev software-properties-common python-software-properties 
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo service tor start
+sudo service openvpn start
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo ppa-purge
+sudo service redis-server restart
+chmod 777 telegram-cli-1222
+chmod 777 anticrash.sh
+export http_proxy = "socks5://127.0.0.1:9050"
+export https_proxy = "socks5://127.0.0.1:9050"
+export ftp_proxy = "socks5://127.0.0.1:9050"
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
+sudo apt-get install lua-lgi
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
+sudo apt-get install libstdc++6
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  tar zxpf luarocks-2.2.2.tar.gz
  cd luarocks-2.2.2
